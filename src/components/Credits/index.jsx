0e6ce080426cf11a0c7183d43266e2c836c56fa4
Jeremy B Merrill <jeremybmerrill@gmail.com>
2020-12-03 10:15:24 -0500

    revise credits

--- src/components/Credits/index.jsx
+++ src/components/Credits/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Credits = () => (
   <div>
     <h4>Credits</h4>
-    <p>Targeting data and images provided by participants in the Ad Observer project. Other ad data via Facebook, provided by the Online Political Ads Transparency Project at the NYU Tandon School of Engineering.</p>
+    <p>Targeting data and images provided by participants in the Ad Observer project at the NYU Tandon School of Engineering.</p>
   </div>
 );
 
