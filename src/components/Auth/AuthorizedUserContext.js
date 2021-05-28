ad589fb1894f980e5e7d3da45a969b93bb7136f9
Paul <mountainmanduke@gmail.com>
2021-05-28 11:28:23 -0400

    remove unneccessary authn and authz context files.

--- src/components/Auth/AuthorizedUserContext.js
+++
@@ -1,6 +0,0 @@
-// For firbase authentication session management
-import React from "react";
-
-const AuthUserContext = React.createContext(null); //using React's Context API
-
-export default AuthUserContext;
